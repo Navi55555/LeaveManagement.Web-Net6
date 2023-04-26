2da74ae468e77e3d510f2f8f238215b61650d0f1
Ivan Costa <costa.ivan55@gmail.com>
2023-04-26 10:48:18 -0300

    Added User Security To App. End of Authentication and Authorization with Identity

---
+++ LeaveManagement.Web/Configuration/Entities/RoleSeedConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using LeaveManagement.Web.Constants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeaveManagement.Web.Configuration.Entities
+{
+    public class RoleSeedConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Id = "f072ffe7-ace9-4ea6-986c-39a821cbb0d7",
+                    Name = Roles.Administrator,
+                    NormalizedName = Roles.Administrator.ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = "4691f418-660f-41ab-a9b6-4bbd24c7c76d",
+                    Name = Roles.User,
+                    NormalizedName = Roles.User.ToUpper()
+                }
+                ); ;
+        }
+    }
+}
--- LeaveManagement.Web/Data/Migrations/20230425125210_AddedDefaultUsersAndRoles.Designer.cs
+++ LeaveManagement.Web/Data/Migrations/20230425125210_AddedDefaultUsersAndRoles.Designer.cs
@@ -4,6 +4,7 @@ using LeaveManagement.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeaveManagement.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230425125210_AddedDefaultUsersAndRoles")]
+    partial class AddedDefaultUsersAndRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -106,41 +109,37 @@ namespace LeaveManagement.Web.Data.Migrations
                         {
                             Id = "f01229c5-d122-4b3e-9082-f497c84eb08e",
                             AccessFailedCount = 0,
-                            ConcurrencyStamp = "7586a4cb-4569-4205-97f7-1ff530fbabd9",
+                            ConcurrencyStamp = "65204123-c827-496e-9c9c-4abb38471658",
                             DateJoined = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             DateOfBirth = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Email = "admin@localhost.com",
-                            EmailConfirmed = true,
+                            EmailConfirmed = false,
                             Firstname = "System",
                             Lastname = "Admin",
                             LockoutEnabled = false,
                             NormalizedEmail = "ADMIN@LOCALHOST.COM",
-                            NormalizedUserName = "ADMIN@LOCALHOST.COM",
-                            PasswordHash = "AQAAAAIAAYagAAAAEM9AtTLQa7BJYn1akdKzlbAQPMQblHlmH1dlebYdmjoWPBxLlGo0cWf+R1wjjjJlnA==",
+                            PasswordHash = "AQAAAAIAAYagAAAAEOgYPNKctjpRdGP5t2a8x+u/GSi1mfXly6I0QH2ZNIiL018UG4IOmIOXNoNXHSvP6Q==",
                             PhoneNumberConfirmed = false,
-                            SecurityStamp = "57b37b70-82bd-40cd-914a-a9eb7df7887f",
-                            TwoFactorEnabled = false,
-                            UserName = "admin@localhost.com"
+                            SecurityStamp = "5753c85b-2030-4092-bc59-8cbf3888c666",
+                            TwoFactorEnabled = false
                         },
                         new
                         {
                             Id = "2cff23f9-e2a3-4ba7-ab5d-a94829389b1c",
                             AccessFailedCount = 0,
-                            ConcurrencyStamp = "7816115d-b21f-4a41-b9c2-119246b23f62",
+                            ConcurrencyStamp = "173aecad-19df-4db8-b609-804e71144b2e",
                             DateJoined = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             DateOfBirth = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Email = "user@localhost.com",
-                            EmailConfirmed = true,
+                            EmailConfirmed = false,
                             Firstname = "System",
                             Lastname = "User",
                             LockoutEnabled = false,
                             NormalizedEmail = "USER@LOCALHOST.COM",
-                            NormalizedUserName = "USER@LOCALHOST.COM",
-                            PasswordHash = "AQAAAAIAAYagAAAAEFrLASWMB2CJ6u7gx5s5gv0H97W0+UPYo+zihob/FIzK8AX0WodtUAwtcmnYN7PU3Q==",
+                            PasswordHash = "AQAAAAIAAYagAAAAEI14BmHun1PaTQB1NX8V6eID8Dbz9d3iQk7ac5GeWeEOcKaSw+3ZC1TdHDRsQ2WT9A==",
                             PhoneNumberConfirmed = false,
-                            SecurityStamp = "0f229cc0-e1ed-447c-b890-a2cdc7030d51",
-                            TwoFactorEnabled = false,
-                            UserName = "user@localhost.com"
+                            SecurityStamp = "3a1ca48b-9982-4e08-9369-94e2e1db5ff6",
+                            TwoFactorEnabled = false
                         });
                 });
 
